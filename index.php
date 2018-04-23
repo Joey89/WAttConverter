b31067397ee966a3dc12fe6c0083357ac26eca7f
Joey <under767@hotmail.com>
2018-04-23 07:46:24 -0400

    Rename index.php to snippet.php

--- index.php
+++
@@ -1,1 +0,0 @@
-<object type="text/html" data="wattconverter.html" width="1000px" height="1000px"></object>
---
+++ snippet.php
@@ -0,0 +1,1 @@
+<object type="text/html" data="wattconverter.html" width="1000px" height="1000px"></object>
